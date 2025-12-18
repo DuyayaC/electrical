10129d9267938c73150eee52557df3397f182803
Duyaya <2371662123@qq.com>
2025-12-19 00:46:52 +0800

    test2

--- 底盘/chassis_control_mode.c
+++ 底盘/chassis_control_mode.c
@@ -40,21 +40,24 @@ static void chassis_static(void)
     float target_vy = 0.0f;
     float target_omega = 0.0f;
     chassis_coordinate_resolution(target_vx, target_vy, target_omega, &chassis_motion);
-
+    chassis_calculation();
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
 //µ×ÅÌ¸úËæ
 static void chassis_follow(float target_vx, float target_vy, float target_omega)
 {
     chassis_coordinate_resolution(target_vx, target_vy, target_omega, &chassis_motion);
-
+    chassis_calculation();
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
 //Ð¡ÍÓÂÝ
 static void chassis_top(float target_omega)
 {
     top_stand_still(target_omega, &chassis_motion);
-
+    chassis_calculation();
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
 //Ð¡ÍÓÂÝÐÐ½ø
@@ -62,7 +65,8 @@ static void chassis_top_moving(float target_vx, float target_vy, float target_om
 {
 
     top_moving(target_vx, target_vy, target_omega, theta, &chassis_motion);
-    
+    chassis_calculation();
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 
 }
 
@@ -72,12 +76,17 @@ static void chassis_calculation(void)
     chassis[1].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[1]);
     chassis[2].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[2]);
     chassis[3].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[3]);
+
+    chassis[0].Calculate(&chassis_motion.motor_output_value[0]);
+    chassis[1].Calculate(&chassis_motion.motor_output_value[1]);
+    chassis[2].Calculate(&chassis_motion.motor_output_value[2]);
+    chassis[3].Calculate(&chassis_motion.motor_output_value[3]);
 }
 
 void chassis_init(void)
 {
-    chassis[0].Init();
-    chassis[1].Init();
-    chassis[2].Init();
-    chassis[3].Init();
+    chassis[0].Init(35.0f, 25.0f, 0.0f, 0.0f, 6000.0f, 16384.0f);
+    chassis[1].Init(35.0f, 25.0f, 0.0f, 0.0f, 6000.0f, 16384.0f);
+    chassis[2].Init(35.0f, 25.0f, 0.0f, 0.0f, 6000.0f, 16384.0f);
+    chassis[3].Init(35.0f, 25.0f, 0.0f, 0.0f, 6000.0f, 16384.0f);
 }
