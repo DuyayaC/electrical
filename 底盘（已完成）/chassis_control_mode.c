ff2365cf7f61c8b3dc5b1a4db90e3d18d5384f28
Duyaya <2371662123@qq.com>
2025-12-23 16:46:06 +0800

    test2

--- 底盘（已完成）/chassis_control_mode.c
+++ 底盘（已完成）/chassis_control_mode.c
@@ -24,14 +24,14 @@ void chassis_control(float target_vx, float target_vy, float target_omega, float
     }
 }
 
-//µ×ÅÌÎÞÁ¦
+//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 static void chassis_zero_force(void)
 {
-    //²»ÊäÈëµçÁ÷
+    //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
     CAN_cmd_chassis(0, 0, 0, 0);
 }
 
-//µ×ÅÌ¾²Ö¹
+//ï¿½ï¿½ï¿½Ì¾ï¿½Ö¹
 static void chassis_static(void)
 {
     float target_vx = 0.0f;
@@ -39,41 +39,45 @@ static void chassis_static(void)
     float target_omega = 0.0f;
     chassis_coordinate_resolution(target_vx, target_vy, target_omega, &chassis_motion);
     chassis_calculation();
-    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], 
+                    chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
-//µ×ÅÌ¸úËæ
+//ï¿½ï¿½ï¿½Ì¸ï¿½ï¿½ï¿½
 static void chassis_follow(float target_vx, float target_vy, float target_omega)
 {
     chassis_coordinate_resolution(target_vx, target_vy, target_omega, &chassis_motion);
     chassis_calculation();
-    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], 
+                    chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
-//Ð¡ÍÓÂÝ
+//Ð¡ï¿½ï¿½ï¿½ï¿½
 static void chassis_top(float target_omega)
 {
     top_stand_still(target_omega, &chassis_motion);
     chassis_calculation();
-    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], 
+                    chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 }
 
-//Ð¡ÍÓÂÝÐÐ½ø
+//Ð¡ï¿½ï¿½ï¿½ï¿½ï¿½Ð½ï¿½
 static void chassis_top_moving(float target_vx, float target_vy, float target_omega, float theta)
 {
 
     top_moving(target_vx, target_vy, target_omega, theta, &chassis_motion);
     chassis_calculation();
-    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
+    CAN_cmd_chassis(chassis_motion.motor_output_value[0], chassis_motion.motor_output_value[1], 
+                    chassis_motion.motor_output_value[2], chassis_motion.motor_output_value[3]);
 
 }
 
 static void chassis_calculation(void)
 {
     chassis[0].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[0]);
-    chassis[1].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[1]);
-    chassis[2].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[2]);
-    chassis[3].Set_Values(chassis_motion.wheel_target_omega[0], m3508.motor_omega[3]);
+    chassis[1].Set_Values(chassis_motion.wheel_target_omega[1], m3508.motor_omega[1]);
+    chassis[2].Set_Values(chassis_motion.wheel_target_omega[2], m3508.motor_omega[2]);
+    chassis[3].Set_Values(chassis_motion.wheel_target_omega[3], m3508.motor_omega[3]);
 
     chassis[0].Calculate(&chassis_motion.motor_output_value[0]);
     chassis[1].Calculate(&chassis_motion.motor_output_value[1]);
