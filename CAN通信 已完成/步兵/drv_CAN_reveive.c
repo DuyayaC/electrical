ff011a82dc7b429d7dc35234f4bbd3c2d0d8dd0d
Duyaya <2371662123@qq.com>
2026-02-10 17:27:19 +0800

    Basic

--- CAN通信 已完成/步兵/drv_CAN_reveive.c
+++ CAN通信 已完成/步兵/drv_CAN_reveive.c
@@ -53,11 +53,7 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
       {
       case CAN_3508_M1_ID:
       case CAN_3508_M2_ID:
-      case CAN_3508_M3_ID:
-      case CAN_3508_M4_ID:
-      case CAN_YAW_MOTOR_ID:
       case CAN_PIT_MOTOR_ID:
-      case CAN_BARREL_MOTOR_ID:
       case CAN_TRIGGER_MOTOR_ID:
       {
         static uint8_t i = 0;
@@ -89,9 +85,6 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
       case CAN_3508_M3_ID:
       case CAN_3508_M4_ID:
       case CAN_YAW_MOTOR_ID:
-      case CAN_PIT_MOTOR_ID:
-      case CAN_BARREL_MOTOR_ID:
-      case CAN_TRIGGER_MOTOR_ID:
       {
         static uint8_t i = 0;
         // get motor id
@@ -193,42 +186,6 @@ void CAN_cmd_chassis(int16_t motor1, int16_t motor2, int16_t motor3, int16_t mot
   HAL_CAN_AddTxMessage(&CHASSIS_CAN, &chassis_tx_message, chassis_can_send_data, &send_mail_box);
 }
 
-#if (Robot_ID == 1)
-void CAN_cmd_friction(int16_t motor1, int16_t motor2, int16_t motor3, int16_t motor4)
-{
-  uint32_t send_mail_box;
-  chassis_tx_message.StdId = CAN_CHASSIS_ALL_ID;
-  chassis_tx_message.IDE = CAN_ID_STD;
-  chassis_tx_message.RTR = CAN_RTR_DATA;
-  chassis_tx_message.DLC = 0x08;
-  chassis_can_send_data[0] = motor1 >> 8;
-  chassis_can_send_data[1] = motor1;
-  chassis_can_send_data[2] = motor2 >> 8;
-  chassis_can_send_data[3] = motor2;
-  chassis_can_send_data[4] = motor3 >> 8;
-  chassis_can_send_data[5] = motor3;
-  chassis_can_send_data[6] = motor4 >> 8;
-  chassis_can_send_data[7] = motor4;
-
-  HAL_CAN_AddTxMessage(&FRICTION_CAN, &chassis_tx_message, chassis_can_send_data, &send_mail_box);
-}
-
-//?????????��?��?��??????????
-void CAN_cmd_shoot(int16_t shoot, int16_t rev)
-{
-  uint32_t send_mail_box;
-  gimbal_tx_message.StdId = 0x1FF;
-  gimbal_tx_message.IDE = CAN_ID_STD;
-  gimbal_tx_message.RTR = CAN_RTR_DATA;
-  gimbal_tx_message.DLC = 0x08;
-  gimbal_can_send_data[4] = (shoot >> 8);
-  gimbal_can_send_data[5] = shoot;
-  gimbal_can_send_data[6] = (rev >> 8);
-  gimbal_can_send_data[7] = rev;
-  HAL_CAN_AddTxMessage(&SHOOT_CAN, &gimbal_tx_message, gimbal_can_send_data, &send_mail_box);
-}
-
-#elif (Robot_ID == 3)
 void CAN_cmd_friction(int16_t motor1, int16_t motor2)
 {
   uint32_t send_mail_box;
@@ -247,7 +204,7 @@ void CAN_cmd_friction(int16_t motor1, int16_t motor2)
 void CAN_cmd_shoot(int16_t shoot, int16_t rev)
 {
   uint32_t send_mail_box;
-  gimbal_tx_message.StdId = 0x1FF;
+  gimbal_tx_message.StdId = CAN_CHASSIS_ALL_ID;
   gimbal_tx_message.IDE = CAN_ID_STD;
   gimbal_tx_message.RTR = CAN_RTR_DATA;
   gimbal_tx_message.DLC = 0x08;
@@ -258,112 +215,52 @@ void CAN_cmd_shoot(int16_t shoot, int16_t rev)
   HAL_CAN_AddTxMessage(&SHOOT_CAN, &gimbal_tx_message, gimbal_can_send_data, &send_mail_box);
 }
 
-#elif (Robot_ID == 4)
-void CAN_cmd_friction(int16_t motor1, int16_t motor2)
-{
-  uint32_t send_mail_box;
-  chassis_tx_message.StdId = CAN_CHASSIS_ALL_ID;
-  chassis_tx_message.IDE = CAN_ID_STD;
-  chassis_tx_message.RTR = CAN_RTR_DATA;
-  chassis_tx_message.DLC = 0x08;
-  chassis_can_send_data[0] = motor1 >> 8;
-  chassis_can_send_data[1] = motor1;
-  chassis_can_send_data[2] = motor2 >> 8;
-  chassis_can_send_data[3] = motor2;
-
-  HAL_CAN_AddTxMessage(&FRICTION_CAN, &chassis_tx_message, chassis_can_send_data, &send_mail_box);
-}
-
-void CAN_cmd_shoot(int16_t shoot, int16_t barrel)
-{
-  uint32_t send_mail_box;
-  gimbal_tx_message.StdId = 0x1FF;
-  gimbal_tx_message.IDE = CAN_ID_STD;
-  gimbal_tx_message.RTR = CAN_RTR_DATA;
-  gimbal_tx_message.DLC = 0x08;
-  gimbal_can_send_data[4] = (shoot >> 8);
-  gimbal_can_send_data[5] = shoot;
-  gimbal_can_send_data[6] = (barrel >> 8);
-  gimbal_can_send_data[7] = barrel;
-  HAL_CAN_AddTxMessage(&SHOOT_CAN, &gimbal_tx_message, gimbal_can_send_data, &send_mail_box);
-}
-#endif
 
 /**
  * @brief          return the yaw 6020 motor data point
  * @param[in]      none
  * @retval         motor data point
  */
-#if (GIMBAL_YAW_CAN == hcan1)
-const motor_measure_t *get_yaw_gimbal_motor_measure_point(void)
-{
-  return &motor_chassis_can1[4];
-}
-#elif (GIMBAL_YAW_CAN == hcan2)
+
+
 const motor_measure_t *get_yaw_gimbal_motor_measure_point(void)
 {
   return &motor_chassis_can2[4];
 }
-#endif
+
 /**
  * @brief          return the pitch 6020 motor data point
  * @param[in]      none
  * @retval         motor data point
  */
-#if (GIMBAL_PITCH_CAN == hcan1)
-
 const motor_measure_t *get_pitch_gimbal_motor_measure_point(void)
 {
   return &motor_chassis_can1[5];
 }
-#elif (GIMBAL_PITCH_CAN == hcan2)
-const motor_measure_t *get_pitch_gimbal_motor_measure_point(void)
-{
-  return &motor_chassis_can2[5];
-}
-#endif
+
 /**
  * @brief          return the trigger 2006 motor data point
  * @param[in]      none
  * @retval         motor data point
  */
-#if (SHOOT_CAN == hcan1)
 const motor_measure_t *get_trigger_motor_measure_point(void)
 {
-  return &motor_chassis_can1[6];
+  return &motor_chassis_can1[2];
 }
-#elif (SHOOT_CAN == hcan2)
-const motor_measure_t *get_trigger_motor_measure_point(void)
-{
-  return &motor_chassis_can2[6];
-}
-#endif
 
 /**
  * @brief          return the chassis 3508 motor data point
  * @param[in]      i: motor number,range [0,3]
  * @retval         motor data point
  */
-#if (CHASSIS_CAN == hcan1)
-const motor_measure_t *get_chassis_motor_measure_point(uint8_t i)
-{
-  return &motor_chassis_can1[(i & 0x03)];
-}
-#elif (CHASSIS_CAN == hcan2)
-const motor_measure_t *get_chassis_motor_measure_point(uint8_t i)
-{
-  return &motor_chassis_can2[(i & 0x03)];
-}
-#endif
 
-#if (FRICTION_CAN == hcan1)
+const motor_measure_t *get_chassis_motor_measure_point(uint8_t i)
+{
+  return &motor_chassis_can2[(i & 0x03)];
+}
+
 const motor_measure_t *get_friction_motor_measure_point(uint8_t i)
 {
   return &motor_chassis_can1[(i & 0x03)];
 }
-#elif (FRICTION_CAN == hcan2)
-const motor_measure_t *get_friction_motor_measure_point(uint8_t i)
-{
-  return &motor_chassis_can2[(i & 0x03)];
-}
-#endif  
+ 
